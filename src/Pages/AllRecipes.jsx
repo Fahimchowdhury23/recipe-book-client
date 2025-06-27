ede0e28d7062ce531c98e7ed6cf9450314754bb6
Fahimchowdhury23 <fahimchowdhury877@gmail.com>
2025-06-27 13:16:55 +0600

    sorting functionality added at all recipes

--- src/Pages/AllRecipes.jsx
+++ src/Pages/AllRecipes.jsx
@@ -5,6 +5,8 @@ import RecipeCard from "./RecipeCard";
 const AllRecipes = () => {
   const recipesData = useLoaderData();
   const [selectedCuisine, setSelectedCuisine] = useState("All");
+  const [sortOrder, setSortOrder] = useState("none");
+
   const cuisines = [
     "All",
     "Italian",
@@ -20,39 +22,87 @@ const AllRecipes = () => {
       ? recipesData
       : recipesData.filter((recipe) => recipe.cuisineType === selectedCuisine);
 
+  const sortedRecipes = [...filteredRecipes].sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.title.toLowerCase().localeCompare(b.title.toLowerCase());
+    } else if (sortOrder === "desc") {
+      return b.title.toLowerCase().localeCompare(a.title.toLowerCase());
+    }
+    return 0;
+  });
+
   return (
     <section>
       <title>All Recipes | Recipe Book</title>
-      <h2 className="text-center pt-6 text-2xl lg:text-3xl font-bold text-secondary drop-shadow mb-2">
+      <h2 className="text-center pt-2 md:pt-4 text-xl md:text-2xl lg:text-3xl font-bold text-secondary drop-shadow md:mb-1">
         All Recipes
       </h2>
-      <p className="border-b-3 w-1/4 mx-auto border-accent/70 mb-6"></p>
+      <p className="border-b-3 w-1/3 md:w-1/4 mx-auto border-accent/70 mb-3 lg:mb-5"></p>
 
-      <div className="w-11/12 lg:w-10/12 mx-auto">
-        <p className="text-lg lg:text-xl font-semibold text-accent mb-2">
-          Cuisine Type :
-        </p>
+      {/* Cuisine Type */}
 
-        <select
-          name="cuisineType"
-          value={selectedCuisine}
-          onChange={(e) => setSelectedCuisine(e.target.value)}
-          className="p-2 lg:p-3 cursor-pointer rounded-xl bg-white text-secondary focus:outline-none focus:ring-1 focus:ring-secondary"
-        >
-          {cuisines.map((type) => (
-            <option
-              className="bg-primary text-lg font-medium"
-              key={type}
-              value={type}
+      <div className="flex justify-center items-center gap-2 md:gap-4">
+        <div className="flex justify-center items-center gap-1 lg:gap-2">
+          <div>
+            <p className="md:text-lg lg:text-xl whitespace-nowrap font-semibold text-accent">
+              Cuisine Type :
+            </p>
+          </div>
+
+          <div>
+            <select
+              name="cuisineType"
+              value={selectedCuisine}
+              onChange={(e) => setSelectedCuisine(e.target.value)}
+              className="p-1 md:p-2 cursor-pointer rounded-lg bg-white text-secondary focus:outline-none focus:ring-1 focus:ring-secondary"
             >
-              {type}
-            </option>
-          ))}
-        </select>
+              {cuisines.map((type) => (
+                <option
+                  className="bg-primary text-lg font-medium"
+                  key={type}
+                  value={type}
+                >
+                  {type}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* Sorting by Title */}
+
+        <div className="flex justify-center items-center gap-1 lg:gap-2">
+          <div>
+            <p className="md:text-lg lg:text-xl whitespace-nowrap font-semibold text-accent">
+              Sort By Title :
+            </p>
+          </div>
+
+          <div>
+            <select
+              name="sorting"
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+              className="p-1 md:p-2 cursor-pointer rounded-lg bg-white text-secondary focus:outline-none focus:ring-1 focus:ring-secondary"
+            >
+              <option className="bg-primary text-lg font-medium" value="none">
+                Default
+              </option>
+
+              <option className="bg-primary text-lg font-medium" value="asc">
+                A to Z
+              </option>
+
+              <option className="bg-primary text-lg font-medium" value="desc">
+                Z to A
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-4 pb-10 w-11/12 xl:w-10/12 mx-auto">
-        {filteredRecipes.map((recipe) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 pt-3 lg:pt-6 pb-10 w-11/12 xl:w-10/12 mx-auto">
+        {sortedRecipes.map((recipe) => (
           <RecipeCard key={recipe._id} recipe={recipe}></RecipeCard>
         ))}
       </div>
--- src/Pages/RecipeCard.jsx
+++ src/Pages/RecipeCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <div className="bg-white rounded-2xl p-2 lg:p-3 shadow-md overflow-hidden flex flex-col">
+    <div className="bg-white rounded-2xl p-2 shadow-md overflow-hidden flex flex-col">
       <Link to={`/recipeDetails/${recipe._id}`}>
         <img
           src={recipe.image}
@@ -12,18 +12,20 @@ const RecipeCard = ({ recipe }) => {
         />
       </Link>
 
-      <div className="p-4 space-y-2 flex-1">
+      <div className="p-2 lg:p-3 space-y-1 lg:space-y-2 flex-1">
         <h2 className="text-xl font-semibold text-black opacity-80">
           {recipe.title}
         </h2>
+
         <p className="text-gray-500">Cuisine: {recipe.cuisineType}</p>
+
         <div className="flex items-center gap-2">
           <img
             src={recipe.photoURL}
             alt={recipe.displayName}
             className="w-10 h-10 rounded-full"
           />
-          <span className="font-medium text-secondary">
+          <span className="font-medium text-black opacity-70">
             By {recipe.displayName}
           </span>
         </div>
